a1236f74d2438aa8beab97554681d0fe831c16d2
JPRubio <rubio-manana@hotmail.com>
2024-10-13 13:01:42 +0200

    Setting up new TComm Tests

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # TCommTests
 
 This used to be part of the TComm library, however now the library is separated from its examples, making for a cleaner implementation.
+
+
+Edit: Now is based on the newest TComm library
---
+++ TCommWinTerminal/main.cpp
@@ -0,0 +1,35 @@
+#include "lib\System\Common.h"
+#include "lib\Communication\Media\UASubscriber.h"
+#include "lib\Communication\Media\ClientHandler.h"
+#include "lib\Communication\Media\WebSocketServer.h"
+#include "lib\Communication\Media\SerialInterface.h"
+#include "FileHandler\FileHandler.h"
+
+TCommServer server;
+opcua::Server uaserver;
+UASubscriber uaSubscriber;
+WebSocketServer webSocketServer;
+
+Entity tCommVerse{nullptr,"tCommVerse"};
+SerialInterface serialInterface{&tCommVerse, "serialInterface"};
+
+int main()
+{
+    if (webSocketServer.begin()) {
+        std::cout << "Server started and listening for connections" << std::endl;
+    } else {
+        std::cout << "Failed to start server" << std::endl;
+    }
+    serialInterface.SetSerialDriver(Serial);
+    uaserver.setApplicationName("TCommVerse");
+    /// Set application URI, default: `urn:open62541.server.application`.
+    uaserver.setApplicationUri("urn:TCommVerse.server.application");
+    uaSubscriber.Xinit(&uaserver);
+    while(1)
+    {
+        clockBits.Run();
+        server.Run();
+        serialInterface.Run();
+        webSocketServer.Run();
+    }
+}

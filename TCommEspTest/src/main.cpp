8a29879b5b67f23ecfa9aaa8ac4391dc00420e06
SinkroniusMaximus <35000863+SinkroniusMaximus@users.noreply.github.com>
2025-03-02 22:35:12 +0100

    Verified serial and wifi socket on esp, after changes

--- TCommEspTest/src/main.cpp
+++ TCommEspTest/src/main.cpp
@@ -7,18 +7,16 @@ TCommClient client;
 Entity root{nullptr,"root"};
 SerialSubscriber serialSub;
 WifiSocket wifiSocket;
-// const char* ssid = "WifiNetwork";           // Replace with wifi network name
-// const char* password = "WifiPassword";      // Replace with actual password
-// const char* host = "ThisIsMyComputerName";  // Replace with your server 'computer name' to connect to
-// const uint16_t port = 1234;
+const char* ssid = "WifiNetwork";           // Replace with wifi network name
+const char* password = "WifiPassword";      // Replace with actual password
+const char* host = "ThisIsMyComputerName";  // Replace with your server 'computer name' to connect to
+const uint16_t port = 1234;
 EquipmentModule equipmentModule{nullptr,"equipmentModule"};
-SerialAdapter serialAdapter(&Serial);
 
 void setup() {
   Serial.begin(230400);//115200);
-  // wifiSocket.Connect(ssid, password, host, port);
-  // serialSub.Xinit(&wifiSocket);
-  serialSub.Xinit(&serialAdapter);
+  wifiSocket.Connect(ssid, password, host, port);
+  serialSub.Xinit(&wifiSocket);
 }
 
 void loop() {
